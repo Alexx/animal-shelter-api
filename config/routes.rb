553ab36e545ef508b4c9484b1a8dbdb15d606078
Alex Siegberg <alex.siegberg@gmail.com>
2019-08-23 15:37:34 -0700

    Add version control for API.

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,24 @@
 Rails.application.routes.draw do
-  root to: "animals#index"
-  resources :animals
 
-  get "/random", to: 'animals#random'
-  get "/all_cats", to: 'animals#allCats'
-  get "/all_dogs", to: 'animals#allDogs'
-  get "/all_rabbits", to: 'animals#allRabbits'
-  get "/largest_cat", to: 'animals#largestCat'
-  get "/largest_dog", to: 'animals#largestDog'
-  get "/largest_rabbit", to: 'animals#largestRabbit'
-  get "/youngest_cat", to: 'animals#youngestCat'
-  get "/youngest_dog", to: 'animals#youngestDog'
-  get "/youngest_rabbit", to: 'animals#youngestRabbit'
-  get "/search", to: 'animals#search'
+  concern :api_base do
+    root to: "animals#index"
+    resources :animals
+
+    get "/random", to: 'animals#random'
+    get "/all_cats", to: 'animals#allCats'
+    get "/all_dogs", to: 'animals#allDogs'
+    get "/all_rabbits", to: 'animals#allRabbits'
+    get "/largest_cat", to: 'animals#largestCat'
+    get "/largest_dog", to: 'animals#largestDog'
+    get "/largest_rabbit", to: 'animals#largestRabbit'
+    get "/youngest_cat", to: 'animals#youngestCat'
+    get "/youngest_dog", to: 'animals#youngestDog'
+    get "/youngest_rabbit", to: 'animals#youngestRabbit'
+    get "/search", to: 'animals#search'
+  end
+
+  namespace :v1 do
+    concerns :api_base
+  end
+  
 end
